ec5b4b648bc2fdf1aa26646b5e4e25b650ae1bb5
djasnive <djasniveyrajaona@gmail.com>
2025-10-20 12:59:24 +0300

    fix: Correction du script d'initialisation du thème pour une application correcte au chargement

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,13 +8,9 @@
         <script>
             (function() {
                 const appearance = '{{ $appearance ?? "system" }}';
-
-                if (appearance === 'system') {
-                    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-
-                    if (prefersDark) {
-                        document.documentElement.classList.add('dark');
-                    }
+                const isDark = appearance === 'dark' || (appearance === 'system' && window.matchMedia('(prefers-color-scheme: dark)').matches);
+                if (isDark) {
+                    document.documentElement.classList.add('dark');
                 }
             })();
         </script>
